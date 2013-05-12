85b8d6f4d484d95bdbadabb06bf280738765e0e4
PENGFEI CHEN <565289656@qq.com>
2013-05-12 13:40:58 +1200

    Remove kony dependency

--- First-Native-Android/src/nz/govt/studylink/mslapp/KonyPDFReader.java
+++ First-Native-Android/src/nz/govt/studylink/mslapp/KonyPDFReader.java
@@ -1,13 +1,5 @@
 package nz.govt.studylink.mslapp;
 
-import java.util.List;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
-
-import com.konylabs.android.KonyMain;
 
 public class KonyPDFReader{
 	
@@ -20,14 +12,14 @@ public class KonyPDFReader{
 //        PackageManager pm = KonyMain.getActivityContext().getPackageManager();
 //        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
 //        
-		Intent pdfIntent;
-		KonyMain context = KonyMain.getActivityContext();
-		if (context != null) {
-			pdfIntent = new Intent(context, AndroidPDFActivity.class);
-			pdfIntent.setAction(Intent.ACTION_VIEW);
-			pdfIntent.setDataAndType(Uri.parse(URL), "application/pdf");
-			context.startActivity(pdfIntent);
-		}
+//		Intent pdfIntent;
+//		KonyMain context = KonyMain.getActivityContext();
+//		if (context != null) {
+//			pdfIntent = new Intent(context, AndroidPDFActivity.class);
+//			pdfIntent.setAction(Intent.ACTION_VIEW);
+//			pdfIntent.setDataAndType(Uri.parse(URL), "application/pdf");
+//			context.startActivity(pdfIntent);
+//		}
 	}
 	
 	public static String returnString() {
